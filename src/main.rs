76f7879b79ff21272668dcd9e8c78c60caa65aca
Justin Barton <bartonjustin@gmail.com>
2021-02-07 21:00:00 +1300

    as secs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::process;
+use std::path::Path;
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    for arg in env::args().skip(1) {
+        let p = Path::new(&arg);
+        if p.exists() && p.is_file() {
+            let f = p.canonicalize().unwrap();
+            let m = fs::metadata(&f).unwrap();
+            println!("{} ({}) {:?} secs", f.display(), arg, m.modified().unwrap().elapsed().unwrap().as_secs());
+        } else {
+            println!("{} - no file", arg);
+            process::exit(1);
+        }
+    }
+    process::exit(0);
 }
