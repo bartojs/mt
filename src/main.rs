a331266518b168204d9b7bb6c1600cab8253a045
Justin Barton <bartonjustin@gmail.com>
2021-02-07 22:35:59 +1300

    chrono

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,26 @@ use std::process;
 use std::path::Path;
 use std::env;
 use std::fs;
+use chrono::prelude::{DateTime, Local};
 
 fn main() {
     for arg in env::args().skip(1) {
         let p = Path::new(&arg);
         if p.exists() && p.is_file() {
             let f = p.canonicalize().unwrap();
-            let m = fs::metadata(&f).unwrap();
-            println!("{} ({}) {:?} secs", f.display(), arg, m.modified().unwrap().elapsed().unwrap().as_secs());
+            let d = fs::metadata(&f).unwrap();
+            let m = d.modified().unwrap();
+            let s = m.elapsed().unwrap().as_secs();
+            if s < 60 {
+                println!("{} ({}) {}secs", f.display(), arg, s);
+            } else if s < 60*60 {
+                println!("{} ({}) {}mins", f.display(), arg, s/60);
+            } else if s < 60*60*60 {
+                println!("{} ({}) {}hrs", f.display(), arg, s/(60*60));
+            } else {
+                let dt: DateTime<Local> = m.into();
+                println!("{} ({}) {}", f.display(), arg, dt.format("%F|%T"));
+            }
         } else {
             println!("{} - no file", arg);
             process::exit(1);
